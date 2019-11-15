b3b253b5cb637fda9b3b1eea64022d513ebe7219
j-navesx <j.naves@campus.fct.unl.pt>
2019-11-15 22:36:02 +0000

    Done loadfiles()

--- trbfinal.asm
+++ trbfinal.asm
@@ -5,9 +5,10 @@ data segment
     dir db "Files\",0
     masterfile db "mastertext.txt",0
     handlers dw 8 dup(?),0
-    nhandler db ?,0
+    nhandler dw ?,0
     masterh dw ?,0
-    buffer db 3000 dup(?),0 
+    buffer db 300 dup(?),0
+    msg1 db "Hi!",0 
 ends
 
 stack segment
@@ -23,7 +24,11 @@ start:
 
     ;CODE
       
+      call cdir
+      call loadfiles
       
+      mov dx, handlers[0]
+      mov bx, handlers[1]
       
       mov ax, 4c00h
       int 21h
@@ -32,28 +37,131 @@ start:
     
     ;PROCS
       
-      loadcdrive proc
+      cdir proc
+        push ax
+        push dx
         mov dx, offset cdrive
         mov ah, 3Bh
         int 21h
+        pop dx
+        pop ax
         ret
-      loadcdrive endp
+      cdir endp
       
-      loadfiles proc
+      filesdir proc
+        push ax
+        push dx
         mov dx, offset dir
         mov ah, 3Bh
         int 21h
-        mov si, offset buffer
-        mov ah, 47h
-        int 21h
-        mov dx, offset masterfile
-        mov al, 0
-        mov ah, 3Dh
-        int 21h
-        mov masterh, ax 
+        pop dx
+        pop ax
+        ret
+      filesdir endp
+      
+      loadfiles proc
+        push ax
+        push dx
+        push di
+        push si
+        
+        call filesdir
+        call loadmaster
+        
+        mov bx, masterh
+        mov cx, 1
+        mov dx, offset buffer
+        call fread
+        mov di, offset buffer[0]
+        call ifword
+        or al,al
+        jz endloadf  
+        
+        inc di
+        mov cx, 199
+        mov dx, di
+        call fread
+        mov di, offset buffer
+        
+        loopfiles:
+          mov si, di
+          mov ax, 0dh
+          repne scasb
+          dec di
+          mov byte ptr di, 0
+          mov dx, si
+          mov al, 00h
+          call fopen
+          push di
+          mov di, nhandler
+          mov handlers[di], ax
+          pop di
+          inc nhandler
+          add di, 2
+          cmp byte ptr di, 0
+          jnz loopfiles 
+        endloadf:
+        
+        pop si
+        pop di
+        pop dx
+        pop ax
         ret
       loadfiles endp
-    
+      
+      fopen proc
+        mov ah, 3Dh
+        int 21h       
+        ret
+      fopen endp
+       
+      fread proc
+        push ax
+        mov ah, 3Fh
+        int 21h
+        pop ax
+        ret
+      fread endp
+      
+      loadmaster proc
+        mov dx, offset masterfile
+        mov ax, 3D02h
+        int 21h
+        jc errorfne:
+        mov masterh, ax
+        jmp endloadmaster
+        errorfne:
+          call masterinit
+        endloadmaster:
+        ret
+      loadmaster endp
+      
+      masterinit proc
+        push cx
+        mov cx,0
+        mov ax, 3C00h
+        int 21h
+        mov masterh, ax
+        pop cx
+        ret  
+      masterinit endp
+      
+      ifword proc
+        mov al, 0
+        cmp byte ptr [di], 'A'
+        jb notword 
+        cmp byte ptr [di], 'Z'
+        jbe wordset
+        cmp byte ptr [di], 'a'
+        jb notword
+        cmp byte ptr [di], 'z'
+        ja notword
+       wordset: 
+        mov al, 1
+       notword:  
+        ret
+      ifword endp
+      
     ;END PROCS
     
         
