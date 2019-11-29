2de406631d5852a43a54b5fedd890c57dd378cf5
rbf29santos <rbf.santos@campus.fct.unl.pt>
2019-11-29 14:52:30 +0000

    Did random()

---
+++ fRandom.asm
@@ -0,0 +1,98 @@
+; multi-segment executable file template.
+
+data segment
+;.....temporary variables......; 
+    sec db ?
+    min db ?
+    hour db ? 
+    secx2 dw ?
+    A dw ?
+    filesize dw ?
+    result dw ?
+    axtest dw ?
+    cxtest dw ?
+;..............................;    
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+;.......GET SISTEM TIME........;
+;OUTPUT:    CH = hour
+;           CL = minute
+;           DH = second
+;           DL = 1/100 second            
+    mov ah, 2Ch
+    int 21h
+    call random        
+;..............................;    
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+    
+random proc
+;.....for testes-temporary.....;
+mov filesize, 30
+mov sec, dh
+mov min, cl
+mov hour, ch
+;..............................;
+
+
+mov dl, 02h
+mov al, dh       ;AX = segundo*2
+mul dl 
+
+
+mov secx2, ax
+
+
+mov ch, 00h      ;CX = 00h:(min)
+                 ;as horas nao nos interessam
+ 
+;checks if 'sec*2' is less or greater than 'min' valeu
+cmp ax, cx
+
+ja sub1 
+                     
+sub cx, ax       ;
+mov cxtest, cx
+mov bx, cx
+mov A, bx
+jmp endsub1
+ 
+sub1:
+sub ax, cx       ;
+mov axtest, ax
+mov bx, ax
+mov A, bx
+
+
+endsub1:
+;checks if filesize is less or greater than last result (BX) 
+cmp bx, filesize
+ja sub2         
+mov ax, 0000h    ;clear registo AX
+mov ax, filesize
+sub ax, bx         ;filesize > BX: AX = AX-BX (AX=filesize-sem o alterar)
+mov result, ax
+jmp endrandom
+
+sub2: 
+sub bx, filesize ;BX > filesize: BX = BX-filesize 
+mov result, bx
+
+endrandom: ret
+   
+endp
+    
+end start 
